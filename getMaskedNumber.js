e70affcf588da609f7180e7dfbc26194481847e6
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:46:07 +0800

    refactor, make more readable

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -2,7 +2,9 @@ export default (number)=>{
   const multipleOf3 = number % 3 === 0
   const multipleOf5 = number % 5 === 0
   let result = ''
-  if(multipleOf3 || number % 10 === 3 || Math.floor(number / 10) === 3){
+  if(multipleOf3
+    || number % 10 === 3
+    || Math.floor(number / 10) === 3){
     result += 'Fizz'
   }
   if(multipleOf5){
