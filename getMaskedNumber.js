010c1ba862bb186ea442786632a0758286e92491
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:37:28 +0800

    refactor, logic for 13 and 23

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -2,7 +2,7 @@ export default (number)=>{
   const multipleOf3 = number % 3 === 0
   const multipleOf5 = number % 5 === 0
   let result = ''
-  if(multipleOf3 || number == 13 || number == 23){
+  if(multipleOf3 || number % 10 === 3){
     result += 'Fizz'
   }
   if(multipleOf5){
