b6012d509fdf1942114ddd79a2eaed271dfdc982
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:32:25 +0800

    refactor, remove some return

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -1,14 +1,15 @@
 export default (number)=>{
   const multipleOf3 = number % 3 === 0
   const multipleOf5 = number % 5 === 0
+  let result = ''
   if(multipleOf3 && multipleOf5){
     return 'FizzBuzz'
   }
   if(multipleOf3){
-    return 'Fizz'
+    result += 'Fizz'
   }
   if(multipleOf5){
-    return 'Buzz'
+    result += 'Buzz'
   }
-  return number
+  return result || number
 }
