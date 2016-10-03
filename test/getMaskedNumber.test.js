c496787b02dbcfa10acef2ad705b700200be99ad
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:09:16 +0800

    ut, for 2, failed

--- test/getMaskedNumber.test.js
+++ test/getMaskedNumber.test.js
@@ -5,4 +5,7 @@ describe('fizz buzz', ()=>{
   it('should be 1 for 1', ()=>{
     expect(getMaskedNumber(1)).to.be.equal(1)
   })
+  it('should be 2 for 2', ()=>{
+    expect(getMaskedNumber(2)).to.be.equal(2)
+  })
 })
