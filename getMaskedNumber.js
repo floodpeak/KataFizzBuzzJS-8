44fb8c00fad5b89cbf42d865080b2a4e93e05d42
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:52:08 +0800

    refactor, create const base 3

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -1,10 +1,11 @@
 export default (number)=>{
-  const multipleOf3 = number % 3 === 0
+  const base3 = 3
+  const multipleOf3 = number % base3 === 0
   const multipleOf5 = number % 5 === 0
   let result = ''
   if(multipleOf3
-    || number % 10 === 3
-    || Math.floor(number / 10) === 3){
+    || number % 10 === base3
+    || Math.floor(number / 10) === base3){
     result += 'Fizz'
   }
   if(multipleOf5
