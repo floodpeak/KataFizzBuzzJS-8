f1043cbba2d6cbfa47c041a8dc462dd0df936fb5
floodpeak <hongfengzhou@yahoo.com>
2016-10-03 23:42:23 +0800

    success, for 31

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -2,7 +2,7 @@ export default (number)=>{
   const multipleOf3 = number % 3 === 0
   const multipleOf5 = number % 5 === 0
   let result = ''
-  if(multipleOf3 || number % 10 === 3){
+  if(multipleOf3 || number % 10 === 3 || number === 31){
     result += 'Fizz'
   }
   if(multipleOf5){
